b8fe498250f51ab2dceb3f2d287b04ae8ab63bf3
karnajitsen <karnajitsen@gmail.com>
2015-05-11 20:24:08 +0200

    upd

--- mgsolve.cpp
+++ mgsolve.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include "Grid.h"
+#include <stdlib.h>
 //#include "Stencil.h"
 #include "stdio.h"
 #include <ctime>
@@ -8,10 +9,10 @@
 #define M_PI 3.14
 #include <cmath>
 
-#define XDOMLOW 0
-#define XDOMHIGH 1
-#define YDOMLOW 0
-#define YDOMHIGH 1
+#define XDOMLOW 0.0
+#define XDOMHIGH 1.0
+#define YDOMLOW 0.0
+#define YDOMHIGH 1.0
 #define TOLERR 0.01
 #define V1 2
 #define V2 1
@@ -28,45 +29,62 @@ inline double gxy(const double x, const double y)
 	return sin(M_PI * x) * sinh(M_PI * y);
 }
 
-Grid** initialize( const double hsize, const int level)
+Grid** initialize(double hsize, const int level)
 {
-	Grid** arrGrid = NULL;
 	int je = level;
-	
+	int gdim = pow(2, je) + 1;
+	Grid** arrGrid = (Grid**)_aligned_malloc(level*sizeof(Grid*), 1024);
 	for (int i = 0; i < level; i++)
 	{
-		int gdim = pow(2, je--) + 1;
+		std::cout << &arrGrid[i] << "\n";
 		arrGrid[i] = new Grid(gdim, gdim, hsize, hsize);
-		Grid grd(gdim, gdim, hsize, hsize);
-		for (int j = 0; j < gdim; j++)
+		for (int j = 0.0; j < gdim; j++)
 		{
 			double k = j*hsize;
-			grd(0, j) = gxy(0,k);
-			grd(j, 0) = gxy(k,0);
-			grd(j, gdim - 1) = gxy(k, gdim - 1);
-			grd(gdim - 1, j) = gxy(gdim - 1, k);
+			double l = (gdim - 1.0)*hsize;
+			(*arrGrid[i])(j, 0) = gxy(k, 0.0);
+			(*arrGrid[i])(0, j) = gxy(0.0, k);
+			(*arrGrid[i])(j, gdim - 1) = gxy(k, l);
+			(*arrGrid[i])(gdim - 1, j) = gxy(l, k);
+
+			//cout << j << " " << gdim - 1 << " " << grd(j, 0) << " " << grd(0, j) << " " << grd(j, gdim - 1) << " " << grd(gdim - 1, j) << "\n\n";
 		}
-		arrGrid[i] = &grd;
+		
+		gdim = pow(2, --je) + 1;
+		hsize *= 2.0;
 	}
 	return arrGrid;
 
 }
 
-inline void calresidual(const Grid * grd, const double * fvec, double * rvec)
+inline void restriction(const Grid * grd, const Grid * fgrd, Grid* rgrid)
 {
-	int xlen = (*grd).getXsize()-1, m=0;
+	int xlen = (*grd).getXsize() - 1, m = 0;
+	Grid tmpgrd(xlen + 1, xlen + 1, 0.0, 0.0);
 	for (int i = 1; i < xlen; i++)
 	{
-		for (int j = 1; j < xlen; j++,m++)
+		for (int j = 1; j < xlen; j++, m++)
 		{
-			rvec[m] = fvec[m] - 4.0*(*grd)(i, j) - (*grd)(i, j-1) - (*grd)(i, j+1) - (*grd)(i-1, j) - (*grd)(i+1, j);
+			tmpgrd(i,j) = (*fgrd)(i,j) - 4.0*(*grd)(i, j) - (*grd)(i, j - 1) - (*grd)(i, j + 1) - (*grd)(i - 1, j) - (*grd)(i + 1, j);
+		}
+	}
+
+	int rlen = (*rgrid).getXsize() - 2;
+	for (int i = 1; i < rlen; i++)
+	{
+		for (int j = 1; j < rlen; j++)
+		{
+			(*rgrid)(i, j) = (tmpgrd(2 * i - 1, 2 * j - 1) + tmpgrd(2 * i - 1, 2 * j + 1) +
+				tmpgrd(2 * i + 1, 2 * j - 1) + tmpgrd(2 * i + 1, 2 * j + 1) +
+				2.0*(tmpgrd(2 * i, 2 * j - 1) + tmpgrd(2 * i, 2 * j + 1) +
+				tmpgrd(2 * i - 1, 2 * j) + tmpgrd(2 * i + 1, 2 * j)) + 4 * tmpgrd(2 * i, 2 * j)) / 16.0;
 		}
 	}
 }
 
-inline void rstrToCoarse( double * rvec, int const totdim)
+inline void restriction(double * rvec, int const totdim)
 {
-	for (int i = 1, j=0; i < totdim; i += 2,j++)
+	for (int i = 1, j = 0; i < totdim; i += 2, j++)
 	{
 		rvec[j] = 0.5 * rvec[i] + 0.25 * (rvec[i - 1] + rvec[i + 1]);
 	}
@@ -75,44 +93,46 @@ inline void rstrToCoarse( double * rvec, int const totdim)
 inline void interpolate(const Grid * grd, Grid * tmpgrd)
 {
 	int len = (*grd).getXsize();
-	int hx = (*grd).getHx()/2.0;
+	int hx = (*grd).getHx() / 2.0;
 	int nlen = len * 2 - 1;
-	tmpgrd = new Grid(nlen,nlen,hx,hx);
+	tmpgrd = new Grid(nlen, nlen, hx, hx);
 	for (int i = 0; i < len; i++)
 	{
 		int k = 2 * i;
 		for (int j = 0; j < len; j++)
 		{
 			int l = 2 * j;
-			(*tmpgrd)(k,l) = (*grd)(i,j);
-			(*tmpgrd)(k + 1, l) = 0.5*((*grd)(i, j) + (*grd)(i+1, j));
-			(*tmpgrd)(k, l + 1) = 0.5*((*grd)(i, j) + (*grd)(i , j+1));
+			(*tmpgrd)(k, l) = (*grd)(i, j);
+			(*tmpgrd)(k + 1, l) = 0.5*((*grd)(i, j) + (*grd)(i + 1, j));
+			(*tmpgrd)(k, l + 1) = 0.5*((*grd)(i, j) + (*grd)(i, j + 1));
 			(*tmpgrd)(k + 1, l + 1) = 0.25*((*grd)(i, j) + (*grd)(i + 1, j) + (*grd)(i, j + 1) + +(*grd)(i + 1, j + 1));
 		}
 	}
 }
 
-inline void rbgs(Grid* grd, const double * fvec, const int iter)
+inline void rbgs(Grid* xgrd, const Grid* fgrd, const int iter)
 {
-	int dimX = (*grd).getXsize();
+	int dimX = (*xgrd).getXsize();
 	//double temp = 0.0;
 
 	for (int i = 0; i < iter; i++)
 	{
-		for (int j = 1; j < dimX; j++)
+		for (int j = 1; j < dimX-1; j++)
 		{
-			for (int k = (j & 1) + 1; k < dimX; k+=2)
+			for (int k = ((j + 1) & 0x1) + 1; k < dimX-1; k += 2)
 			{
-				(*grd)(j, k) = 0.25*((*grd)(j + 1, k) + (*grd)(j - 1, k) + (*grd)(j, k + 1) + (*grd)(j, k - 1));
+				std::cout << "888888\n ";
+				(*xgrd)(j, k) = 0.25*((*xgrd)(j + 1, k) + (*xgrd)(j - 1, k) + (*xgrd)(j, k + 1) + (*xgrd)(j, k - 1));
 			}
-			
+
 		}
 
-		for (int j = 1; j < dimX; j++)
+		for (int j = 1; j < dimX-1; j++)
 		{
-			for (int k = j & 1; k < dimX; k += 2)
+			for (int k = (j & 0x1)+1; k < dimX-1; k += 2)
 			{
-				(*grd)(j, k) = 0.25*((*grd)(j + 1, k) + (*grd)(j - 1, k) + (*grd)(j, k + 1) + (*grd)(j, k - 1));
+				std::cout << "99999";
+				(*xgrd)(j, k) = 0.25*((*xgrd)(j + 1, k) + (*xgrd)(j - 1, k) + (*xgrd)(j, k + 1) + (*xgrd)(j, k - 1));
 			}
 
 		}
@@ -120,16 +140,16 @@ inline void rbgs(Grid* grd, const double * fvec, const int iter)
 
 }
 
-inline void calNorm(Grid* grd, const double * fvec, double* norm)
+inline void calNorm(Grid* xgrd, const Grid * fgrd, double* norm)
 {
-	int dimX = (*grd).getXsize();
+	int dimX = (*xgrd).getXsize();
 	double r = 0.0;
-	
+
 	for (int j = 1; j < dimX; j++)
 	{
 		for (int k = (j & 1) + 1; k < dimX; k += 2)
 		{
-			r = 0.25*((*grd)(j + 1, k) + (*grd)(j - 1, k) + (*grd)(j, k + 1) + (*grd)(j, k - 1));
+			r = 0.25*((*xgrd)(j + 1, k) + (*xgrd)(j - 1, k) + (*xgrd)(j, k + 1) + (*xgrd)(j, k - 1));
 			*norm += r*r;
 		}
 
@@ -139,7 +159,7 @@ inline void calNorm(Grid* grd, const double * fvec, double* norm)
 	{
 		for (int k = j & 1; k < dimX; k += 2)
 		{
-			r = 0.25*((*grd)(j + 1, k) + (*grd)(j - 1, k) + (*grd)(j, k + 1) + (*grd)(j, k - 1));
+			r = 0.25*((*xgrd)(j + 1, k) + (*xgrd)(j - 1, k) + (*xgrd)(j, k + 1) + (*xgrd)(j, k - 1));
 			*norm += r*r;
 		}
 
@@ -150,7 +170,7 @@ inline void calNorm(Grid* grd, const double * fvec, double* norm)
 
 int main(int argc, char** argv)
 {
-  
+
 	std::cout << "1";
 	if (argc < 3)
 	{
@@ -158,57 +178,58 @@ int main(int argc, char** argv)
 		exit(0);
 	}
 
-	std::cout << argv[1] << " " << argv[2];
+	//std::cout << argv[1] << " " << argv[2];
 	clock_t tim;
 	int level = atoi(argv[1]);
 	int vcycle = atoi(argv[2]);
-	size_t gdim = pow(2,level) + 1;
+	size_t gdim = pow(2, level) + 1;
 	size_t vdim = gdim - 2;
 	double oldnorm = 0.0, newnorm = 0.0, convrate = 0.0;
-	double hsize = (XDOMHIGH - XDOMLOW) / (gdim - 1);
+	double hsize = (XDOMHIGH - XDOMLOW) / (gdim - 1.0);
 	size_t totdim = vdim*vdim;
-	Grid ** grids = initialize(hsize,level);
-	double * fvec = NULL, *rvec = NULL;
-	fvec = new double[totdim]();
+
+	Grid ** xGrids = initialize(hsize, level);
+	Grid ** fGrids = initialize(hsize, level);
+	//fvec = new double[totdim]();
 
 	tim = clock();
-	std::cout << "2";
+	
+
 	for (int i = 0; i < vcycle; i++)
-	{		
-		rbgs(grids[0],fvec,V1);
-		calresidual(grids[0], fvec, rvec);
-		rstrToCoarse(rvec, totdim);
+	{
+
+		std::cout << "3";
+		rbgs(xGrids[0], fGrids[0], V1);
+		std::cout << "3";
+		restriction(xGrids[0], fGrids[0],xGrids[1]);
+		//rstrToCoarse(rvec, totdim);
 		for (int j = 1; j < level; j++)
 		{
 			vdim = vdim - 2;
 			totdim = vdim*vdim;
-			rbgs(grids[j], rvec, V1);
-			calresidual(grids[j], rvec, rvec);
-			rstrToCoarse(rvec, totdim);
+			rbgs(xGrids[j], fGrids[j], V1);
+			restriction(xGrids[j], fGrids[0], xGrids[j+1]);
+			//rstrToCoarse(rvec, totdim);
 		}
 
-		rbgs(grids[level+1], rvec, V1);
+		rbgs(xGrids[level + 1], fGrids[level + 1], V1);
 		Grid * tmpgrid = NULL;
-		interpolate(grids[level + 1], tmpgrid);
-		//free(grids[level + 1]);
+		interpolate(xGrids[level + 1], tmpgrid);
 		for (int j = level; j > 0; j--)
-		{  
+		{
 			tmpgrid = NULL;
-			rbgs(grids[j], rvec, V2);
-			interpolate(grids[j], tmpgrid);
-			//free(grids[j]);
+			rbgs(xGrids[j], fGrids[j], V2);
+			interpolate(xGrids[j], tmpgrid);
 		}
 		oldnorm = newnorm;
-		calNorm(grids[0], fvec, &newnorm);
+		calNorm(xGrids[0], fGrids[0], &newnorm);
 		if (oldnorm != 0.0)
 			convrate = newnorm / oldnorm;
-		//if (norm < TOLERR)
-			//break;
-
+		
 		std::cout << "Residual after " << i + 1 << "V-Cycle = " << newnorm;
 		std::cout << "Covergence rate after " << i + 1 << "V-Cycle = " << convrate;
 	}
-	
+
 	tim = clock() - tim;
 
 	std::cout << "Time spend for two V - cycles= " << tim;
