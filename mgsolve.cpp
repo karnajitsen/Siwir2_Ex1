53218baf337bf6d9c8b832bd8614cc1bc8d3e980
karnajitsen <karnajitsen@gmail.com>
2015-05-14 22:26:12 +0200

    upd

--- mgsolve.cpp
+++ mgsolve.cpp
@@ -206,7 +206,7 @@ int main(int argc, char** argv)
     {
 		size_t jl = 0;
 		
-		for (jl = 0; jl < level - 1; jl++)
+		/*for (jl = 0; jl < level - 1; jl++)
 		{
 			std::string fnames11 = std::string("data/Downb4_") + std::string(to_string(jl)) + std::string(".txt");
 			std::ofstream	fOutsolt11(fnames11);
@@ -219,24 +219,24 @@ int main(int argc, char** argv)
 				//fOut << std::endl;
 				fOutsolt11 << std::endl;
 			}
-			fOutsolt11.close();
+			fOutsolt11.close();*/
 
 
 			smooth(xGrids[jl], fGrids[jl], V1);
 			restriction(xGrids[jl], fGrids[jl], fGrids[jl + 1]);
 
-			std::string fnames1 = std::string("data/Down_") + std::string(to_string(jl)) + std::string(".txt");
-			std::ofstream	fOutsolt1(fnames1);
-			for (int y = 0; y < (*xGrids[jl]).getXsize(); ++y) {
-				for (int x = 0; x < (*xGrids[jl]).getXsize(); ++x) {
+			//std::string fnames1 = std::string("data/Down_") + std::string(to_string(jl)) + std::string(".txt");
+			//std::ofstream	fOutsolt1(fnames1);
+			//for (int y = 0; y < (*xGrids[jl]).getXsize(); ++y) {
+			//	for (int x = 0; x < (*xGrids[jl]).getXsize(); ++x) {
 
-					fOutsolt1 << x* (*xGrids[jl]).getHx() << "\t" << y* (*xGrids[jl]).getHx() << "\t" << (*xGrids[jl])(x, y) << std::endl;
-					//fOutsolt1 << x*hsize << "\t" << y*hsize << "\t" << sGrid(x, y) << std::endl;
-				}
-				//fOut << std::endl;
-				fOutsolt1 << std::endl;
-			}
-			fOutsolt1.close();
+			//		fOutsolt1 << x* (*xGrids[jl]).getHx() << "\t" << y* (*xGrids[jl]).getHx() << "\t" << (*xGrids[jl])(x, y) << std::endl;
+			//		//fOutsolt1 << x*hsize << "\t" << y*hsize << "\t" << sGrid(x, y) << std::endl;
+			//	}
+			//	//fOut << std::endl;
+			//	fOutsolt1 << std::endl;
+			//}
+			//fOutsolt1.close();
 		}
 		
 		for (size_t j = level - 1; j > 0; j--)
@@ -248,8 +248,8 @@ int main(int argc, char** argv)
 
 			std::string fnames1 = std::string("data/Up_") + std::string(to_string(j)) + std::string(".txt");
 			std::ofstream	fOutsolt1(fnames1);
-			for (int y = 0; y < (*xGrids[j]).getXsize(); ++y) {
-				for (int x = 0; x < (*xGrids[j]).getXsize(); ++x) {
+			for (int y = 0; y < (*xGrids[j-1]).getXsize(); ++y) {
+				for (int x = 0; x < (*xGrids[j-1]).getXsize(); ++x) {
 
 					fOutsolt1 << x*hsize << "\t" << y*hsize << "\t" << (*xGrids[j])(x,y) << std::endl;
 					//fOutsolt1 << x*hsize << "\t" << y*hsize << "\t" << sGrid(x, y) << std::endl;
