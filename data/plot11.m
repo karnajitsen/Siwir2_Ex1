aae3e60382fb23629b0d6bdc61971e2a9073f3e0
karnajitsen <karnajitsen@gmail.com>
2015-05-14 21:31:37 +0200

    upd

---
+++ data/plot11.m
@@ -0,0 +1,18 @@
+%for i=0:6
+A= importdata('Up_1.txt');
+M = A(:,1);
+N = A(:,2);
+P = A(:,3);
+tri2 = delaunay(M,N);
+figure
+trimesh(tri2,M,N,P);
+title "Down";
+%end
+% B= importdata('exactsolution.txt');
+% M = B(:,1);
+% N = B(:,2);
+% P = B(:,3);
+% tri2 = delaunay(M,N);
+% figure
+% trimesh(tri2,M,N,P);
+% title 'Exact Solution';
