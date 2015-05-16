6600afd4bb7b38543b24bf74c1ce7a4a3368f01b
karnajitsen <karnajitsen@gmail.com>
2015-05-16 11:30:32 +0200

    upd

--- data/Plot1.m
+++ data/Plot1.m
@@ -1,13 +1,9 @@
-A = importdata('exactsolution.txt');
+A = importdata('solution.txt');
 X = A(:,1);
 Y =A(:,2);
 Z = A(:,3);
 
 tri = delaunay(X,Y);
 trisurf(tri,X,Y,Z);
-colorbar
+color
 
-P = peaks(40);
-%C = del2(P);
-surf(P,C)
-title('surf(P,del2(P))')
