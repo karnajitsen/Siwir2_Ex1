025cce94388aaf30ce0ebb7d10939e233389b55e
karnajitsen <karnajitsen@gmail.com>
2015-05-18 01:30:10 +0200

    upd

--- mgsolve.cpp
+++ mgsolve.cpp
@@ -83,12 +83,12 @@ void restriction(const Grid * xgrd, const Grid * fgrd, Grid* rgrid)
 
 		if (*ndflag == 0)
 		{
-			tmpgrd(0, i) = (*fgrd)(0, i) + (2.0 / hx) + 2.0*alpha*((*xgrd)(1, i)) //+ beta * ((*xgrd)(0, i + 1)
-				//+ (*xgrd)(0, i - 1)) 
+			tmpgrd(0, i) = (*fgrd)(0, i) + (2.0 / hx) + 2.0*alpha*((*xgrd)(1, i)) + beta * ((*xgrd)(0, i + 1)
+				+ (*xgrd)(0, i - 1)) 
 				- (*xgrd)(0, i) * center ;
 
-			tmpgrd(xlen, i) = (*fgrd)(xlen, i) + (2.0 / hx) + 2.0*alpha*((*xgrd)(xlen - 1, i)) //+ beta * ((*xgrd)(xlen, i + 1)
-				//+ (*xgrd)(xlen, i - 1)) 
+			tmpgrd(xlen, i) = (*fgrd)(xlen, i) + (2.0 / hx) + 2.0*alpha*((*xgrd)(xlen - 1, i)) + beta * ((*xgrd)(xlen, i + 1)
+				+ (*xgrd)(xlen, i - 1)) 
 				- (*xgrd)(xlen, i) * center ;
 		}
 	}
@@ -247,14 +247,14 @@ inline void resdualNorm(const Grid* xgrd, const Grid * fgrd, double* norm)
 
 		if (*ndflag == 0)
 		{
-			r = hx*hy*(*fgrd)(0, j) + (2.0 * hx) + 2.0*alpha*((*xgrd)(1, j)) //+ beta * ((*xgrd)(0, j + 1)
-				//+ (*xgrd)(0, j - 1)) 
+			r = hx*hy*(*fgrd)(0, j) + (2.0 * hx) + 2.0*alpha*((*xgrd)(1, j)) + beta * ((*xgrd)(0, j + 1)
+				+ (*xgrd)(0, j - 1)) 
 				- (*xgrd)(0, j) * center;
 
 			*norm += r*r;
 
-			r = hx*hy*(*fgrd)(dimX, j) + (2.0 * hx) + 2.0*alpha*((*xgrd)(dimX - 1, j)) //+ beta * ((*xgrd)(dimX, j + 1)
-				//+ (*xgrd)(dimX, j - 1)) 
+			r = hx*hy*(*fgrd)(dimX, j) + (2.0 * hx) + 2.0*alpha*((*xgrd)(dimX - 1, j)) + beta * ((*xgrd)(dimX, j + 1)
+				+ (*xgrd)(dimX, j - 1)) 
 				- (*xgrd)(dimX, j) * center;
 
 			*norm += r*r;
